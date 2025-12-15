a73bab2a99c15e9ec356fb7cc43070ea4f286c41
joshcullen <joshcullen10@gmail.com>
2025-12-15 17:37:54 -0500

    Created first script to write simple Zarr file from ERDDAP dataset

---
+++ 01_Create simple Zarr file.py
@@ -0,0 +1,52 @@
+
+### Example code to write a small Zarr file based on a netCDF ###
+
+import xarray as xr
+import matplotlib.pyplot as plt
+import datetime as dt
+
+
+# Load previously downloaded CMEMS data
+base = "https://coastwatch.pfeg.noaa.gov/erddap/griddap"
+dataset_id = "erdQCwindproducts3day"   # 3-day composite (daily time steps)
+url = f"{base}/{dataset_id}"
+
+ds = xr.open_dataset(url, decode_times=True)
+ds
+
+# Define time period of interest
+t0 = "2024-01-01"
+t1 = dt.date.today().strftime("%Y-%m-%d")  #today's date
+
+# Subset data over time and space
+dc = ds['ekman_upwelling'].sel(
+  latitude=slice(50, 30),  #ymax needs to be listed first
+  longitude=slice(-130, -115), 
+  time=slice(t0, t1))
+
+dc.sel(time=dc.time.max()).plot()
+plt.close()
+dc.size
+
+dz = dc.chunk({
+    "time": 365  # uniform chunks except last chunk (Zarr requirement)
+})
+
+# Print chunk sizes per dim (time, altitude, lat, long)
+dz.chunks
+dz.sizes
+
+# Write Zarr file
+zarr_path = "./zarr_simple"
+%%time
+dz.to_zarr(
+    zarr_path,
+    mode="w",
+    consolidated=False
+)
+#took 16 sec to run
+
+
+
+# Try to read in Zarr file now
+new_ds = xr.open_zarr("zarr_simple/")
