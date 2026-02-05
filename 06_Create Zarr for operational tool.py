2b900c2a64fce7f0bf5747d1fc7b628d96c873cf
joshcullen <joshcullen10@gmail.com>
2026-02-05 13:21:49 -0500

    Modified script to upload Zarr to GCS bucket to account for different data dims

--- 06_Create Zarr for operational tool.py
+++ 06_Create Zarr for operational tool.py
@@ -6,11 +6,68 @@ import zarr
 import gcsfs
 import matplotlib.pyplot as plt
 import datetime as dt
+import glob
+import pandas as pd
+import os
+from pathlib import Path
+
+# Grab all .nc files, but only if '2026' is NOT in the name
+path = Path("/Users/joshcullen/Documents/UCSC_NOAA_Projects/Projects/CEG_operationalization/data_processing/TopPredatorWatch/rasters")
+filtered_files = [str(f) for f in path.glob("*.nc") if "2026" not in f.name]
+
+ds = xr.open_mfdataset(filtered_files, combine="by_coords")
+print(ds)
+
+# Chunk DataArray
+# ds = ds.chunk({
+#     "time": 31  # uniform chunks except last chunk (Zarr requirement)
+# })
+# ds.chunks
+# print(ds)
+
+
+# Function to preprocess NPP files (that don't have time stored in file)
+def add_time_from_filename(ds):
+    # Extract the filename from the source attribute xarray adds automatically
+    filepath = ds.encoding["source"]
+    filename = os.path.basename(filepath)
+    
+    # Logic to extract date: Adjust the slice/logic based on your filename format
+    # Example: "weather_2023-10-01.nc" -> "2023-10-01"
+    date_str = filename.split('_')[1].split('.')[0]
+    file_time = pd.to_datetime(date_str)
+    
+    # Expand dims and assign the coordinate
+    # This turns a 2D (lat, lon) into 3D (time, lat, lon)
+    return ds.expand_dims(time=[file_time])
+
 
 
 # Load in netCDFs from leatherback iSDM project
-file_path = "/Users/joshcullen/Documents/UCSC_NOAA_Projects/Projects/CEG_operationalization/data_processing/TopPredatorWatch/rasters/*.nc"
-ds = xr.open_mfdataset(file_path)
+file_path1 = "/Users/joshcullen/Documents/UCSC_NOAA_Projects/Projects/CEG_operationalization/data_processing/TopPredatorWatch/rasters/*_2026-*.nc"
+files1 = glob.glob(file_path1)
+# ds = xr.open_mfdataset(file_path)
+
+exclude_list = ["PPupper200m"]
+filtered_files1 = [f for f in files1 if not any(x in f for x in exclude_list)]
+
+# Combine daily files together
+ds1 = xr.open_mfdataset(filtered_files1)
+print(ds1)
+
+#npp
+ds_npp = xr.open_mfdataset("/Users/joshcullen/Documents/UCSC_NOAA_Projects/Projects/CEG_operationalization/data_processing/TopPredatorWatch/rasters/*PPupper200m_2026-*.nc",
+                           preprocess=add_time_from_filename)
+print(ds_npp)
+
+# Merge daily data
+ds_daily = xr.merge([ds1, ds_npp])
+print(ds_daily)
+
+
+# Merge daily w/ large files
+ds_full = xr.merge([ds, ds_daily])
+
 
 # View size of dataset
 print(f"Size in TB: {ds.nbytes / 1e12:.2f} TB")
@@ -18,16 +75,16 @@ ds.sizes
 print(ds)
 
 
-ds['analysed_sst'].isel(time=-1).plot()
+ds_full['CHL'].isel(time=398).plot()
 plt.close()
 
 
 
 # Chunk DataArray
-ds = ds.chunk({
+ds_full = ds_full.chunk({
     "time": 31  # uniform chunks except last chunk (Zarr requirement)
 })
-ds.chunks
+ds_full.chunks
 
 
 
@@ -40,9 +97,9 @@ zarr_path = "gs://esd-climate-ecosystems-dev/zarr_cmems"
 
 # Write to cloud bucket
 %%time
-ds.to_zarr(
+ds_full.to_zarr(
     zarr_path,
-    mode="w-",
+    mode="w",
     consolidated=False,
     storage_options={"token": "google_default"}
 )
